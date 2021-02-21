63e74dae5c17e18102f1d048e3a6529bbc38ef9d
Amelie010307 <78184572+Amelie010307@users.noreply.github.com>
2021-02-21 14:02:33 -0500

    Add files via upload

---
+++ parcial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <vector>
+main()
+{
+	
+	cout<<"\n\n\n\n\n BIENVENIDOS A LA TIENDA DE AMELIE";
+	
+	
+	
+	
+	
+}
+\[]
